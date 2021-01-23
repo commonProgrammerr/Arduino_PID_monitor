aadd7d7974a37bba486081cf60d261ae58db1714
André <agver@ecomp.poli.br>
2021-01-23 19:27:53 -0300

    bugfix: compile faill (class constructor)

--- Robot/src/main.cpp
+++ Robot/src/main.cpp
@@ -7,26 +7,27 @@
  *
 */
 
-
 #include <Arduino.h>
-#include "PID_v1.h"
+#include "../include/Arduino-PID-Library/PID_v1.h"
 
+
+// pinos
 #define TRIG_PIN 2
 #define ECHO_PIN 3
 
 // sginal bytes 
-#define READY   0x0f // '►' 
-#define STOP    0x24 // '$'
-#define HEADER  0x3c // '<' 
-#define TAIL    0x3e // '>' 
-#define RECIVE  0x40 // '@'
-#define SEND    0x13 // '‼'
+#define READY   '►' // 0x0f
+#define STOP    '$' // 0x24
+#define HEADER  '<' // 0x3c
+#define TAIL    '>' // 0x3e
+#define RECIVE  '@' // 0x40
+#define SEND    '‼' // 0x13
 
 // RECIVE/SEND bytes
-#define SET_DISTANCE  0x21 // '!' declara a distância "objetiva" 
-#define OK            0x5f // '_' byte de confirmação
-#define ERROR         0x3a // ':' byte de erro 
-#define SPLITER       0x3b // ';' byte de separação de valores
+#define SET_DISTANCE  '!' // 0x21  declara a distância "objetiva" 
+#define OK            '_' // 0x5f  byte de confirmação
+#define ERROR         ':' // 0x3a  byte de erro 
+#define SPLITER       ';' // 0x3b  byte de separação de valores
 
 
 // constantes
@@ -35,29 +36,38 @@
 #define Ki 5
 #define Kd 1
 
-class Motor;
+class Motor {
+  private:
+    int portaA;
+    int portaB;
+
+  public: 
+    Motor(int portaA, int portaB);
+    void move(double pow);
+};
+
 float mensure();
 void send(String info);
+void read();
 
-String output = "";
+
+
 double setpoint, distance, motors_power;
 bool is_pause = true;
-Motor* left_motor;
-Motor* right_motor;
+Motor left_motor(10, 9);
+Motor right_motor(5, 6);
 
-PID myPID(&distance, &motors_power, &setpoint, Kp, Ki, Kd, P_ON_M, DIRECT);
+PID pid(&distance, &motors_power, &setpoint, Kp, Ki, Kd, P_ON_M, DIRECT);
 
 void setup() {
 
   Serial.begin(2000000);
-  left_motor = new Motor(10, 9);
-  right_motor = new Motor(5, 6);
-  pinMode(TRIG, OUTPUT);
-  pinMode(ECHO, INPUT);
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
   
-  setpoint = 4.0
-  myPID.SetOutputLimits(-255, 255);
-  myPID.SetMode(AUTOMATIC);
+  setpoint = 4.0;
+  pid.SetOutputLimits(-255, 255);
+  pid.SetMode(AUTOMATIC);
 }
 
 void loop() {
@@ -69,19 +79,23 @@ void loop() {
   
   distance = mensure();
   
-  myPID.Compute();
+  pid.Compute();
   
-  output = output.concat(distance).concat(SPLITER).concat(motors_power);
+  String output = "";
+  output = output.concat(String(distance));
+  output = output.concat(String(SPLITER));
+  output = output.concat(String(motors_power));
+  
   send(output);
-  output = ""
+  output = "";
   
-  left_motor->move(motors_power);
-  right_motor->move(motors_power); 
+  left_motor.move(motors_power);
+  right_motor.move(motors_power); 
 }
 
 void read() {
-  Serial.readBytesUntil(RECIVE);
-  Serial.readBytesUntil(HEADER);
+  Serial.readStringUntil(RECIVE);
+  Serial.readStringUntil(HEADER);
   String data = Serial.readStringUntil(TAIL);
 
   bool erro = (data.indexOf(ERROR) >= 0);
@@ -91,17 +105,17 @@ void read() {
   if(erro || stop) is_pause = true;
     
   if(set_distance) {
-    data.remove(READY)
-    data.remove(STOP)
-    data.remove(HEADER)
-    data.remove(TAIL)
-    data.remove(RECIVE)
-    data.remove(SEND)
-    data.remove(SET_DISTANCE)
-    data.remove(OK)
-    data.remove(ERROR)
-    data.remove(SPLITER)
-    setpoint = data.toDouble()
+    data.remove(READY);
+    data.remove(STOP);
+    data.remove(HEADER);
+    data.remove(TAIL);
+    data.remove(RECIVE);
+    data.remove(SEND);
+    data.remove(SET_DISTANCE);
+    data.remove(OK);
+    data.remove(ERROR);
+    data.remove(SPLITER);
+    setpoint = data.toDouble();
   }
 
 }
@@ -119,43 +133,36 @@ float mensure() {
     delayMicroseconds(2);// espera o final do ruino.
 
     // Emite ondas ultrassonicas por 10 microssegundos (10 * 1^-10000 segundos)
-    digitalWrite(TRIG, HIGH); 
+    digitalWrite(TRIG_PIN, HIGH); 
     delayMicroseconds(10); 
-    digitalWrite(TRIG, LOW);
+    digitalWrite(TRIG_PIN, LOW);
     
     // Espera a leitura da primeira onda e calcula o a distancia de acodo com o intervalo de tempo obitido.
-    return pulseIn(ECHO, HIGH) /  K/ 2; // o tempo é dividido por 2 pois a onda vai até o objeto e volta, percorrendo o caminho 2 vezes.
+    return pulseIn(ECHO_PIN, HIGH) /  K/ 2; // o tempo é dividido por 2 pois a onda vai até o objeto e volta, percorrendo o caminho 2 vezes.
 }
 
-class Motor {
-  private:
-    int PortaA;
-    int PortaB;
+Motor::Motor(int portaA, int portaB) {
+  this->portaA = portaA;
+  this->portaB = portaB;
+  pinMode(this->portaA, OUTPUT); 
+  pinMode(this->portaB, OUTPUT); 
+}
 
-  public: 
-    Motor::Motor(int PortaA, int PortaB) {
-      this->PortaA = PortaA;
-      this->PortaB = PortaB;
-      pinMode(this->PortaA, OUTPUT); 
-      pinMode(this->PortaB, OUTPUT); 
-    }
+void Motor::move(double pow) {
 
-    void move(double pow) {
+  if(pow > 0.0 && pow <= 255.0) {
 
-      if(pow > 0.0 && pow <= 255.0) {
+    analogWrite(this->portaA, pow);
+    analogWrite(this->portaB, 0);
+  
+  } else if (pow < 0.0 && pow >= -255) {
+  
+    analogWrite(this->portaA, 0);
+    analogWrite(this->portaB, pow * -1);
 
-        analogWrite(this->portaA, pow);
-        analogWrite(this->portaB, 0);
-      
-      } else if (pow < 0.0 && pow >= -255) {
-      
-        analogWrite(this->portaA, 0);
-        analogWrite(this->portaB, pow * -1);
-
-      } else {
-      
-        analogWrite(this->portaA, 0);
-        analogWrite(this->portaB, 0);
-      }
-    }
+  } else {
+  
+    analogWrite(this->portaA, 0);
+    analogWrite(this->portaB, 0);
+  }
 }
